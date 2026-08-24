3a419b49afcd068a8a8f9ba3e35beaa0e5c41025
Sebastian Thomas <68529971+html-proof@users.noreply.github.com>
2026-08-25 04:30:16 +0530

    Delete flutter_app directory

--- flutter_app/android/app/src/main/kotlin/com/musichub/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.musichub.app
-
-import com.ryanheise.audioservice.AudioServiceActivity
-
-class MainActivity : AudioServiceActivity()
--- flutter_app/lib/core/audio/playback_file_probe.dart
+++
@@ -1,2 +0,0 @@
-export 'playback_file_probe_stub.dart'
-    if (dart.library.io) 'playback_file_probe_io.dart';
